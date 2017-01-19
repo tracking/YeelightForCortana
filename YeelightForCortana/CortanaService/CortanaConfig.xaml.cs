31e3a7d8c6efb13d040a918e9806976b8bf577b4
Tracking <tracking@live.cn>
2017-01-19 18:04:10 +0800

    Coding

--- YeelightForCortana/CortanaService/CortanaConfig.xaml.cs
+++ YeelightForCortana/CortanaService/CortanaConfig.xaml.cs
@@ -49,96 +49,7 @@ namespace CortanaService
             //        //writer.WriteAsync();
             //    }
             //}
-            await xmlgen();
-        }
-        private async Task xmlgen()
-        {
-            var localFolder = ApplicationData.Current.LocalFolder;
-            var vcdFile = await localFolder.CreateFileAsync(@"Voice.xml", CreationCollisionOption.ReplaceExisting);
-            var configFile = await localFolder.CreateFileAsync(@"setting.json", CreationCollisionOption.OpenIfExists);
-            JArray commands;
-
-            using (var stream = new StreamReader(await configFile.OpenStreamForReadAsync()))
-            {
-                string data = await stream.ReadToEndAsync();
-                commands = (JArray)JObject.Parse(data)["commands"];
-            }
-
-            XNamespace xnVoiceCommands = "http://schemas.microsoft.com/voicecommands/1.2";
-            XDocument xdoc = new XDocument();
-            XElement VoiceCommands = new XElement(xnVoiceCommands + "VoiceCommands");
-            XElement CommandSet = new XElement(xnVoiceCommands + "CommandSet");
-            XElement AppName = new XElement(xnVoiceCommands + "AppName");
-            XElement AppExample = new XElement(xnVoiceCommands + "Example");
-
-            // xdoc
-            xdoc.Declaration = new XDeclaration("1.0", "utf-8", "");
-            xdoc.Add(VoiceCommands);
-
-            // VoiceCommands
-            VoiceCommands.SetAttributeValue("xmlns", xnVoiceCommands);
-            VoiceCommands.Add(CommandSet);
-
-            // CommandSet
-            CommandSet.SetAttributeValue(XNamespace.Xml + "lang", "zh-cn");
-            CommandSet.SetAttributeValue("Name", "AdventureWorksCommandSet_zh-cn");
-            CommandSet.Add(AppName);
-            CommandSet.Add(AppExample);
-
-            // AppName
-            AppName.SetValue("你好小娜");
-
-            // AppExample
-            AppExample.SetValue("你好小娜");
-
-            for (int i = 0; i < commands.Count; i++)
-            {
-                XElement Command = new XElement(xnVoiceCommands + "Command");
-                XElement CommandExample = new XElement(xnVoiceCommands + "Example");
-                XElement ListenFor = new XElement(xnVoiceCommands + "ListenFor");
-                XElement Feedback = new XElement(xnVoiceCommands + "Feedback");
-                XElement VoiceCommandService = new XElement(xnVoiceCommands + "VoiceCommandService");
-
-                // Command
-                Command.SetAttributeValue("Name", i);
-                Command.Add(CommandExample);
-                Command.Add(ListenFor);
-                Command.Add(Feedback);
-                Command.Add(VoiceCommandService);
-
-                // Example
-                CommandExample.SetValue(commands[i]["listenFor"]);
-
-                // ListenFor
-                ListenFor.SetValue(commands[i]["listenFor"]);
-
-                // Feedback
-                Feedback.SetValue(commands[i]["feedBack"]);
-
-                // VoiceCommandService
-                VoiceCommandService.SetAttributeValue("Target", "YeelightVoiceCommandService");
-
-                // 加入
-                CommandSet.Add(Command);
-            }
-
-            using (var stream = await vcdFile.OpenAsync(FileAccessMode.ReadWrite))
-            {
-                Stream s = stream.AsStreamForWrite();
-                Debug.WriteLine(s.Length);
-                xdoc.Save(s);
-                await s.FlushAsync();
-                //System.Xml.XmlWriterSettings settings = new System.Xml.XmlWriterSettings();
-                //settings.Async = true;
-                //using (var writer = System.Xml.XmlWriter.Create(s, settings))
-                //{
-                //    xdoc.WriteTo(writer);
-                //    await writer.FlushAsync();
-                //}
-            }
-            // 安装语音命令文件
-            //Windows.Storage.StorageFile vcdStorageFile = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(@"YeelightVoiceCommands.xml");
-            await Windows.ApplicationModel.VoiceCommands.VoiceCommandDefinitionManager.InstallCommandDefinitionsFromStorageFileAsync(vcdFile);
+            await SettingHelper.CortanaSetting();
         }
 
         // 加载中
@@ -179,31 +90,7 @@ namespace CortanaService
 
         private async Task SaveSetting(JObject setting)
         {
-            var localFolder = ApplicationData.Current.LocalFolder;
-            var file = await localFolder.CreateFileAsync(@"setting.json", CreationCollisionOption.OpenIfExists);
-            JObject config;
-
-            using (var stream = new StreamReader(await file.OpenStreamForReadAsync()))
-            {
-                string data = await stream.ReadToEndAsync();
-
-                if (string.IsNullOrEmpty(data))
-                {
-                    config = new JObject();
-                    config["commands"] = new JArray();
-                }
-                else
-                {
-                    config = JObject.Parse(data);
-                }
-            }
-
-           ((JArray)config["commands"]).Add(setting);
-
-            using (var stream = new StreamWriter(await file.OpenStreamForWriteAsync()))
-            {
-                await stream.WriteAsync(config.ToString());
-            }
+            await SettingHelper.AddCommand(setting);
         }
 
         private async void button_Click_1(object sender, RoutedEventArgs e)
--- YeelightForCortana/CortanaService/YeelightVoiceCommandService.cs
+++ YeelightForCortana/CortanaService/YeelightVoiceCommandService.cs
@@ -18,88 +18,124 @@ namespace CortanaService
     /// </summary>
     public sealed class YeelightVoiceCommandService : IBackgroundTask
     {
-        private BackgroundTaskDeferral serviceDeferral;
-        VoiceCommandServiceConnection voiceServiceConnection;
+        // 后台任务名
+        private readonly string TRIGGER_DETAIL_NAME = "YeelightVoiceCommandService";
 
+        // 后台任务延迟
+        private BackgroundTaskDeferral serviceDeferral;
+        // 语音服务连接
+        private VoiceCommandServiceConnection voiceServiceConnection;
+
+        // 后台任务入口
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
+            // 获取后台任务相关的详细信息
+            var triggerDetails = (AppServiceTriggerDetails)taskInstance.TriggerDetails;
+
+            // 保存后台任务延迟实例
             this.serviceDeferral = taskInstance.GetDeferral();
 
-            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
-            voiceServiceConnection = VoiceCommandServiceConnection.FromAppServiceTriggerDetails(triggerDetails);
-
-            VoiceCommand voiceCommand = await voiceServiceConnection.GetVoiceCommandAsync();
-
-            IList<Yeelight> yeelightList = await YeelightUtils.SearchDeviceAsync(2000);
-            var userMessage = new VoiceCommandUserMessage();
-            VoiceCommandResponse response;
-
-            int index = Convert.ToInt32(voiceCommand.CommandName);
-
-            var localFolder = ApplicationData.Current.LocalFolder;
-            var configFile = await localFolder.CreateFileAsync(@"setting.json", CreationCollisionOption.OpenIfExists);
-            JArray commands;
-
-            using (var stream = new StreamReader(await configFile.OpenStreamForReadAsync()))
+            // 无法获取后台任务或非本后台任务
+            if (triggerDetails == null || triggerDetails.Name != TRIGGER_DETAIL_NAME)
             {
-                string data = await stream.ReadToEndAsync();
-                commands = (JArray)JObject.Parse(data)["commands"];
+                return;
             }
 
-
-            JObject command = (JObject)commands[index];
-            JArray deviceIds = (JArray)command["deviceIds"];
-            LightAction action = (LightAction)Enum.Parse(typeof(LightAction), command["action"]["action"].ToString());
-            List<Yeelight> deviceList = new List<Yeelight>();
-
-            foreach (string item in deviceIds)
+            try
             {
-                var light = yeelightList.First(k => item == k.Id);
+                // 获取语音服务连接
+                this.voiceServiceConnection = VoiceCommandServiceConnection.FromAppServiceTriggerDetails(triggerDetails);
+                // 语音命令完成事件
+                this.voiceServiceConnection.VoiceCommandCompleted += OnVoiceCommandCompleted;
 
-                if (light == null)
-                    return;
+                // 获取当前的语音命令
+                VoiceCommand voiceCommand = await this.voiceServiceConnection.GetVoiceCommandAsync();
+                // 声明用户信息（用于回应）
+                var userMessage = new VoiceCommandUserMessage();
 
-                deviceList.Add(light);
-            }
+                // 命令名
+                string commandName = voiceCommand.CommandName;
+                // 获取电灯列表
+                var lightList = await YeelightUtils.SearchDeviceAsync();
 
-            foreach (var item in deviceList)
-            {
-                int bright;
-
-                switch (action)
+                if (lightList.Count == 0)
+                    userMessage.DisplayMessage = userMessage.SpokenMessage = "没有找到可操作的电灯";
+                else
                 {
-                    case LightAction.PowerOn:
-                        await item.SetPower(YeelightPower.on);
-                        break;
-                    case LightAction.PowerOff:
-                        await item.SetPower(YeelightPower.off);
-                        break;
-                    case LightAction.BrightUp:
-                        bright = Convert.ToInt32(command["action"]["value"].ToString());
-                        bright = bright + item.Bright;
-                        bright = bright > 100 ? 100 : bright;
-                        bright = bright < 1 ? 1 : bright;
-                        await item.SetBright(bright);
-                        break;
-                    case LightAction.BrightDown:
-                        bright = Convert.ToInt32(command["action"]["value"].ToString());
-                        bright = item.Bright - bright;
-                        bright = bright > 100 ? 100 : bright;
-                        bright = bright < 1 ? 1 : bright;
-                        await item.SetBright(bright);
-                        break;
+                    // 命令索引
+                    int index = Convert.ToInt32(voiceCommand.CommandName);
+                    // 读取设置
+                    JObject config = await SettingHelper.LoadSetting();
+                    // 命令列表
+                    JArray commands = (JArray)config["commands"];
+                    // 当前的命令
+                    JObject command = (JObject)commands[index];
+                    // 对应的设备ID
+                    JArray deviceIds = (JArray)command["deviceIds"];
+                    // 动作
+                    LightAction action = (LightAction)Enum.Parse(typeof(LightAction), command["action"]["action"].ToString());
+                    // 操作设备列表
+                    List<Yeelight> deviceList = new List<Yeelight>();
+
+                    // 找到对应设备
+                    foreach (string item in deviceIds)
+                    {
+                        var light = lightList.First(k => item == k.Id);
+
+                        if (light == null)
+                            return;
+
+                        deviceList.Add(light);
+                    }
+
+                    // 遍历执行操作
+                    foreach (var item in deviceList)
+                    {
+                        int bright;
+
+                        switch (action)
+                        {
+                            case LightAction.PowerOn:
+                                await item.SetPower(YeelightPower.on);
+                                break;
+                            case LightAction.PowerOff:
+                                await item.SetPower(YeelightPower.off);
+                                break;
+                            case LightAction.BrightUp:
+                                bright = Convert.ToInt32(command["action"]["value"].ToString());
+                                bright = bright + item.Bright;
+                                bright = bright > 100 ? 100 : bright;
+                                bright = bright < 1 ? 1 : bright;
+                                await item.SetBright(bright);
+                                break;
+                            case LightAction.BrightDown:
+                                bright = Convert.ToInt32(command["action"]["value"].ToString());
+                                bright = item.Bright - bright;
+                                bright = bright > 100 ? 100 : bright;
+                                bright = bright < 1 ? 1 : bright;
+                                await item.SetBright(bright);
+                                break;
+                        }
+                    }
                 }
+
+                // 声明语音回应
+                VoiceCommandResponse response = VoiceCommandResponse.CreateResponse(userMessage);
+                // 回应小娜
+                await voiceServiceConnection.ReportSuccessAsync(response);
             }
+            finally
+            {
+                if (this.serviceDeferral != null)
+                    this.serviceDeferral.Complete();
+            }
+        }
 
-            //userMessage.DisplayMessage = String.Format("已为你打开{0}盏电灯", yeelightList.Count);
-            //userMessage.SpokenMessage = userMessage.DisplayMessage;
-
-            response = VoiceCommandResponse.CreateResponse(userMessage);
-
-            await voiceServiceConnection.ReportSuccessAsync(response);
-            response = VoiceCommandResponse.CreateResponse(userMessage);
-
-            await voiceServiceConnection.ReportSuccessAsync(response);
+        // 语音命令完成事件
+        private void OnVoiceCommandCompleted(VoiceCommandServiceConnection sender, VoiceCommandCompletedEventArgs args)
+        {
+            if (this.serviceDeferral != null)
+                this.serviceDeferral.Complete();
         }
     }
 }
