db1a56422acb530faf8d5b9d94d9d806e120b7fe
Tracking <tracking@live.cn>
2017-02-19 00:08:57 +0800

    Revert "Revert "Coding""
    This reverts commit 376ed3434adb4dcec643b37337038d4ded98544f.

--- YeelightForCortana/YeelightForCortana/MainPage.xaml.cs
+++ YeelightForCortana/YeelightForCortana/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using YeelightForCortana.ViewModel;
 
 namespace YeelightForCortana
 {
@@ -26,6 +27,36 @@ namespace YeelightForCortana
         {
             this.InitializeComponent();
             this.PageStyleInit();
+            this.PageSampleDataInit();
+        }
+
+        /// <summary>
+        /// 页面示例数据初始化
+        /// </summary>
+        private void PageSampleDataInit()
+        {
+            this.DataContext = new MainPageViewModel()
+            {
+                // 左区分组列表
+                DeviceGroupList = new DeviceGroupList()
+                {
+                    new DeviceGroup() { Id = "0", Name="全部"},
+                    new DeviceGroup() { Id = "1", Name="客厅"},
+                    new DeviceGroup() { Id = "2", Name="厕所"},
+                    new DeviceGroup() { Id = "3", Name="走廊"},
+                    new DeviceGroup() { Id = "4", Name="书房"},
+                    new DeviceGroup() { Id = "5", Name="卧室"}
+                },
+                // 中区操作类型
+                CommandTypeList = new CommandTypeList()
+                {
+                    new CommandType() {Id = 0, Name="全部" },
+                    new CommandType() {Id = 1, Name="开灯" },
+                    new CommandType() {Id = 2, Name="关灯" },
+                    new CommandType() {Id = 3, Name="切换颜色" },
+                    new CommandType() {Id = 4, Name="调整亮度" }
+                }
+            };
         }
 
         /// <summary>
@@ -41,6 +72,19 @@ namespace YeelightForCortana
             var titleBar = ApplicationView.GetForCurrentView().TitleBar;
             titleBar.BackgroundColor = titleBar.ButtonBackgroundColor = Colors.Black;
             titleBar.ForegroundColor = titleBar.ButtonForegroundColor = Colors.White;
+        }
+
+        // 设备组列表鼠标点击事件
+        private void LB_DeviceGroupList_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            var position = e.GetCurrentPoint(sender as UIElement);
+
+            // 如果是鼠标右键点击
+            if (position.Properties.IsRightButtonPressed)
+            {
+                // 显示右键菜单
+                this.MF_DeviceGroupMenu.ShowAt(this.LB_DeviceGroupList, position.Position);
+            }
         }
     }
 }
