7182db6d1d38669743a981f1d1312a0006e7fa78
Ethan Lau <Ethan Lau@VS2015_DEV>
2017-02-18 17:23:10 +0800

    Coding

--- YeelightForCortana/YeelightForCortana/MainPage.xaml.cs
+++ YeelightForCortana/YeelightForCortana/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -24,10 +25,22 @@ namespace YeelightForCortana
         public MainPage()
         {
             this.InitializeComponent();
+            this.PageStyleInit();
+        }
 
+        /// <summary>
+        /// 页面样式初始化
+        /// </summary>
+        private void PageStyleInit()
+        {
             // 设置大小
             ApplicationView.PreferredLaunchViewSize = new Size(1000, 640);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            // 设置标题栏颜色
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = titleBar.ButtonBackgroundColor = Colors.Black;
+            titleBar.ForegroundColor = titleBar.ButtonForegroundColor = Colors.White;
         }
     }
 }
---
+++ YeelightForCortana/YeelightForCortana/ViewModel/DeviceGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeelightForCortana.ViewModel
+{
+    public class DeviceGroup : INotifyPropertyChanged
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
